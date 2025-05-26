51d87c93e87f1d5e8cbfe5adc8ad29a2e1e1e866
Bobrich228 <Glyph228@yandex.ru>
2025-05-26 13:44:05 +1000

    Update lab15v2.c

--- lab15v2.c
+++ lab15v2.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+void print_forward(char n[], int i){
+    if (n[i] == '\0')
+        return;
+    print_forward(n, i + 1);
+    printf("%c ", n[i]);
+}
+void print_backward(char n[], int i){
+    if (n[i] == '\0')
+        return;
+    printf("%c ", n[i]);
+    print_backward(n, i + 1);
+}
+int main(){
+    char n[256];
+    scanf("%s", &n);
+    print_backward(n,0);
+    printf("\n");
+    print_forward(n, 0);
+    printf("\n");
+    return 0;
+}
